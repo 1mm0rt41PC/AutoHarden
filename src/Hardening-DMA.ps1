a94af03ebe78d2c9032e89451ba5e335f35b77e2
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2022-08-26 16:48:26 +0200

    Update Hardening-DMA.ps1

--- src/Hardening-DMA.ps1
+++ src/Hardening-DMA.ps1
@@ -1,2 +1,5 @@
 # Disable new DMA devices when this computer is locked
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\FVE" /v DisableExternalDMAUnderLock /t REG_DWORD /d 1 /f
+# WARNING, an Windows update, will break any laptop.
+# In case of laptop locked down by dma, the computer will show an error "unable to read memory at 0x...." and the laptop will not be able to reboot.
+# Fix: Disable DMS by disabling secureboot or VT then remove this key and disable dma
+# reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\FVE" /v DisableExternalDMAUnderLock /t REG_DWORD /d 1 /f
