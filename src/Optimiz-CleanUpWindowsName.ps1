2f5f1b66300c9643b4090d69a8c7e2051cab1af1
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2022-09-15 10:17:43 +0200

    Update Optimiz-CleanUpWindowsName.ps1

--- src/Optimiz-CleanUpWindowsName.ps1
+++ src/Optimiz-CleanUpWindowsName.ps1
@@ -1,28 +1,17 @@
-$finalUser='Administrateur'
-try{
-	if( -Not (New-Object System.Security.Principal.NTAccount($finalUser)).Translate([System.Security.Principal.SecurityIdentifier]).value.EndsWith('-500') ){
-		$finalUser='Invité'
-	}
-}catch{
-	$finalUser='Administrator'
-	if( -Not (New-Object System.Security.Principal.NTAccount($finalUser)).Translate([System.Security.Principal.SecurityIdentifier]).value.EndsWith('-500') ){
-		$finalUser='Guest'
-	}
-}
-
-
-function killfakename( $file ){
+function killfakename( $file )
+{
 	echo "$file ========="
-	#takeown.exe /f $file
-	icacls.exe "$file" /setowner $env:username | Out-Null
-	remove-item -Force $file | Out-Null
+	$null | out-file "$file.acl"
+	icacls "$file" /save "$file.acl"
+	icacls.exe "$file" /setowner $env:username
+	remove-item -Force $file
 	echo '' | Out-File $file
-	icacls.exe "$file" /setowner $finalUser | Out-Null
-	attrib +s +h $file | Out-Null
-	#(Get-Acl $file).Owner
+	icacls "$file.acl" /save "$file"
+	attrib +s +h $file
+	remove-item -Force "$file.acl"
+	(Get-Acl $file).Owner
 	#(Get-Acl $file).Access
 }
-
 killfakename 'C:\Users\desktop.ini'
 killfakename 'C:\Program Files\desktop.ini'
 killfakename 'C:\Program Files (x86)\desktop.ini'
